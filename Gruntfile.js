08dd3ae735663619c5eb2db7701b2de2ca5fa862
quanzhiyuan <958201599@qq.com>
2017-03-06 10:19:05 +0800

    grunt 配置

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,150 +1,61 @@
-/*!
- * Bootstrap's Gruntfile
- * http://getbootstrap.com
- * Copyright 2013-2016 Twitter, Inc.
- * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
- */
-
-module.exports = function (grunt) {
-  'use strict';
-
-  // Force use of Unix newlines
-  grunt.util.linefeed = '\n';
-
-  RegExp.quote = function (string) {
-    return string.replace(/[-\\^$*+?.()|[\]{}]/g, '\\$&');
-  };
-
-  var fs = require('fs');
-  var path = require('path');
-  var generateGlyphiconsData = require('./grunt/bs-glyphicons-data-generator.js'); // 加载字体库
-  var BsLessdocParser = require('./grunt/bs-lessdoc-parser.js');
-  var getLessVarsData = function () {                                              // 使用pug/jade转化成HTML模板
-    var filePath = path.join(__dirname, 'less/variables.less');
-    var fileContent = fs.readFileSync(filePath, { encoding: 'utf8' });
-    var parser = new BsLessdocParser(fileContent);
-    return { sections: parser.parseFile() };
-  };
-  var generateRawFiles = require('./grunt/bs-raw-files-generator.js');
-  var generateCommonJSModule = require('./grunt/bs-commonjs-generator.js');        // 使文件已commonjs规范加载
-  var configBridge = grunt.file.readJSON('./grunt/configBridge.json', { encoding: 'utf8' }); // 定义grunt 的一些参数
-
-
-  // Project configuration.
-  grunt.initConfig({
-
-    // Metadata.
-    pkg: grunt.file.readJSON('package.json'),
-    banner: '/*!\n' +
-            ' * kingdee-UED quanzhiyuan\n' +
-            ' */\n',
-    jqueryCheck: configBridge.config.jqueryCheck.join('\n'),
-    jqueryVersionCheck: configBridge.config.jqueryVersionCheck.join('\n'),
-
-    // Task configuration.
-    clean: {                    // 清空代码
-      dist: 'dist',
-      docs: 'docs/dist'
-    },
-
-    jshint: {                   // js规范
-      options: {
-        jshintrc: 'js/.jshintrc'
-      },
-      grunt: {
-        options: {
-          jshintrc: 'grunt/.jshintrc'
+module.exports = function(grunt){
+    grunt.initConfig({
+        pkg:grunt.file.readJSON('package.json'),
+        uglify:{
+            options:{
+                banner:'/*zyquan*/'
+            },
+            build:{
+                src:'Scripts/product/test.js',
+                dest:'dist/js/<%=pkg.name%>.min.js'
+            }
         },
-        src: ['Gruntfile.js', 'package.js', 'grunt/*.js']
-      },
-      core: {
-        src: 'js/*.js'
-      },
-        project :{
-        src :'project/js/*.js'
-       },
-      test: {
-        options: {
-          jshintrc: 'project/js/tests/unit/.jshintrc'
+        cssmin:{
+            options:{
+                banner:'/*测试数据*/'
+            },
+            build:{
+                src:'Content/product/*.css',
+                dest:'dist/js/<%=pkg.name%>.min.css'
+            }
         },
-        src: 'js/tests/unit/*.js'
-      },
-      assets: {
-        src: ['docs/assets/js/src/*.js', 'docs/assets/js/*.js', '!docs/assets/js/*.min.js']
-      }
-    },
-    autoprefixer: {      // 自动添加浏览器前缀
-      options: {
-        browsers: configBridge.config.autoprefixerBrowsers
-      },
-      project: {
-        options: {
-          map: true
+        jshint:{
+            options:{
+
+            },
+
         },
-        src: 'project/css/ued.css'
-      }
-    },
+        postcss:{
+            options:{
+                processors:[
+                    require('autoprefixer')
+                ]
+            },
+            dist:{
+                src:'Content/product/*.css'
+            }
+        },
+        browserSync:{
+            dev:{
+                bsFiles:{
+                    src:[
+                        'Content/*/*.css',
+                        'Views/*/*.html'
+                    ],
+                    options:{
+                        watchTask:true,
+                        server:'./'
+                    }
+                }
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-postcss');
+    grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-browser-sync');
 
-    csslint: {  // css 代码检查
-      options: {
-        csslintrc: 'less/.csslintrc'
-      },
-      project: [
-        'project/css/*.css'
-      ]
-    },
+    grunt.registerTask('default',['uglify','cssmin','postcss:dist']);
 
-
-    htmllint: {   // HTML验证工具
-      options: {
-        ignore: [
-          'Attribute "autocomplete" not allowed on element "button" at this point.',
-          'Attribute "autocomplete" is only allowed when the input type is "color", "date", "datetime", "datetime-local", "email", "hidden", "month", "number", "password", "range", "search", "tel", "text", "time", "url", or "week".',
-          'Element "img" is missing required attribute "src".'
-        ]
-      },
-      src: '_gh_pages/**/*.html'
-    },
-
-    exec: {
-      npmUpdate: {
-        command: 'npm update'
-      }
-    }
-
-  });
-
-
-  // These plugins provide necessary tasks.
-  require('load-grunt-tasks')(grunt, { scope: 'devDependencies' });
-  require('time-grunt')(grunt);
-
-  // Docs HTML validation task
-
-
-  var runSubset = function (subset) {
-    return !process.env.TWBS_TEST || process.env.TWBS_TEST === subset;
-  };
-  var isUndefOrNonZero = function (val) {
-    return val === undefined || val !== '0';
-  };
-
-
-
-
-  // Default task.
-
-
-  grunt.registerTask('build-glyphicons-data', function () { generateGlyphiconsData.call(this, grunt); });
-
-  // task for building customizer
-  grunt.registerTask('build-raw-files', 'Add scripts/less files to customizer.', function () {
-    var banner = grunt.template.process('<%= banner %>');
-    generateRawFiles(grunt, banner);
-  });
-  grunt.registerTask('default', ['clean:dist', 'copy:fonts']);
-  grunt.registerTask('autoprefixer', ['autoprefixer:project']);
-  grunt.registerTask('test-js', ['jshint:project']);
-  grunt.registerTask('validate-html', [ 'htmllint']);
-  grunt.registerTask('test-css', ['csslint:project']);
-};
+}
